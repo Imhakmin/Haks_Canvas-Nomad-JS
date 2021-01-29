5f6562c889c92b81ec2e1d904990aad88bd4d753
Imhakmin <55011131+Imhakmin@users.noreply.github.com>
2021-01-29 15:00:28 +0900

    Updated version 1.0.0

--- app.js
+++ app.js
@@ -46,14 +46,59 @@ function handleColorClick(event) {
     ctx.fillStyle = color;
 }
 
+function handleRangeChange(event) {
+    const stroke = event.target.value;
+    ctx.lineWidth = stroke;
+}
+
+function handleModeClick(event) {
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    }else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+}
+
+function handleCM(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick() {
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS";
+    link.click();
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
-    //canvas.addEventListener("click",handleCanvasClick);
-    //canvas.addEventListener("contextmenu",handleCM);
+    canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu",handleCM);
 }
 
 Array.from(colors).forEach(color =>
     color.addEventListener("click",handleColorClick));
+
+if(range) {
+    range.addEventListener("input",handleRangeChange);
+}
+
+if(mode) {
+    mode.addEventListener("click",handleModeClick);
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click",handleSaveClick);
+}
